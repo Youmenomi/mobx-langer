94099f658d2a969686831600f51efeeb18b314e4
dean <>
2021-01-11 14:28:44 +0800

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -25,7 +25,7 @@ export default [].concat(
   build(
     input,
     {
-      file: 'dist/umd/mobx-langer.umd.js',
+      file: `dist/umd/${pkg.name}.umd.js`,
       format: 'umd',
       name: 'mobxLanger',
       globals: {
